34e0af806ab32409c22db4151fb52b226c74a64c
LoveSongs <157166083+lovesongsapp@users.noreply.github.com>
2024-07-23 11:01:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -281,7 +281,7 @@ document.getElementById('share-icon').addEventListener('click', function() {
     if (navigator.share) {
         navigator.share({
             title: videoData.title,
-            text: `Confira este vídeo: ${videoData.title}`,
+            text: `Permita que essa música toque sua alma: Confira este vídeo: ${videoData.title}`,
             url: shareUrl,
         }).then(() => {
             console.log('Compartilhamento bem-sucedido');
