51f982a50dd85592d19b77c08d85e92b3e803357
LoveSongs <157166083+lovesongsapp@users.noreply.github.com>
2024-07-26 04:53:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,163 +1,295 @@
-// Importações do Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-import { getFirestore, collection, doc, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+let player;
+let maxQuality = 'large'; // Definir resolução máxima
+let minQuality = 'medium'; // Definir resolução mínima
+let isPlaying = false;
+let isShuffle = false;
+let mode = 'repeat'; // 'repeat', 'repeat_one', 'shuffle'
+let progressBar, currentTimeDisplay, durationDisplay;
+let playlistData = [];
+let sharedVideoId = null;
 
-// Configuração do Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyCOed1uYtjzkR1OpYS6z3-sbIVPQW6MohM",
-  authDomain: "lovesongs-1285e.firebaseapp.com",
-  projectId: "lovesongs-1285e",
-  storageBucket: "lovesongs-1285e.appspot.com",
-  messagingSenderId: "940749066428",
-  appId: "1:940749066428:web:4bc067e8721fa576fe40b9",
-  measurementId: "G-HLGV34NCB0"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-// Função para mostrar mensagem de sucesso
-function showSuccessMessage(message) {
-  const successMessageElement = document.getElementById('success-message');
-  successMessageElement.textContent = message;
-  successMessageElement.classList.remove('hidden');
-  successMessageElement.classList.add('show');
-  
-  setTimeout(() => {
-    successMessageElement.classList.remove('show');
-    successMessageElement.classList.add('hidden');
-  }, 3000); // Ocultar mensagem após 3 segundos
+function setVideoQuality(quality) {
+    player.setPlaybackQuality(quality);
 }
 
-// Função para login com Google
-async function loginWithGoogle() {
-  const provider = new GoogleAuthProvider();
-  try {
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-    console.log('Usuário conectado com o Google com sucesso:', user);
-    showSuccessMessage('Usuário conectado com o Google com sucesso!');
-    setTimeout(() => {
-      window.location.href = '/login/sucesso.html';
-    }, 3500); // Redirecionar após a mensagem ser exibida
-  } catch (error) {
-    displayErrorMessage(error.message);
-  }
-}
-
-// Função para registrar usuário
-async function registerUser(email, password, username) {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-
-    await setDoc(doc(collection(db, 'users'), user.uid), {
-      username: username,
-      email: email,
-      createdAt: serverTimestamp()
-    });
-
-    console.log('Usuário cadastrado com sucesso:', user);
-    showSuccessMessage('Usuário cadastrado com sucesso!');
-    setTimeout(() => {
-      window.location.href = '/login/sucesso.html';
-    }, 3500); // Redirecionar após a mensagem ser exibida
-  } catch (error) {
-    displayErrorMessage(error.message);
-  }
-}
-
-// Função para login do usuário
-async function loginUser(email, password) {
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-    console.log('Usuário logado com sucesso:', user);
-    showSuccessMessage('Usuário logado com sucesso!');
-    setTimeout(() => {
-      window.location.href = '/login/sucesso.html';
-    }, 3500); // Redirecionar após a mensagem ser exibida
-  } catch (error) {
-    displayErrorMessage(error.message);
-  }
-}
-
-// Função para redefinir senha
-async function resetPassword(email) {
-  try {
-    await sendPasswordResetEmail(auth, email);
-    showSuccessMessage('Enviamos um link para redefinir sua senha. Verifique seu email.');
-  } catch (error) {
-    displayErrorMessage(error.message);
-  }
-}
-
-// Adiciona listeners para os formulários e botões
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('register-form').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const username = document.getElementById('register-username').value;
-    const email = document.getElementById('register-email').value;
-    const password = document.getElementById('register-password').value;
-    await registerUser(email, password, username);
-  });
-
-  document.getElementById('login-form').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const email = document.getElementById('login-email').value;
-    const password = document.getElementById('login-password').value;
-    await loginUser(email, password);
-  });
-
-  document.getElementById('google-login').addEventListener('click', async () => {
-    await loginWithGoogle();
-  });
-
-  document.getElementById('reset-password').addEventListener('click', async () => {
-    const email = document.getElementById('login-email').value;
-    if (email) {
-      await resetPassword(email);
+document.addEventListener('DOMContentLoaded', function() {
+    progressBar = document.getElementById('progress');
+    currentTimeDisplay = document.getElementById('current-time');
+    durationDisplay = document.getElementById('duration');
+    
+    // Verifique se todos os elementos DOM necessários estão presentes
+    if (progressBar && currentTimeDisplay && durationDisplay) {
+        onYouTubeIframeAPIReady();
     } else {
-      alert('Por favor, insira seu email para redefinir sua senha.');
+        console.error('Um ou mais elementos DOM não foram encontrados.');
     }
-  });
-
-  document.getElementById('show-register').addEventListener('click', (event) => {
-    event.preventDefault();
-    document.getElementById('login-form').style.display = 'none';
-    document.getElementById('register-form').style.display = 'block';
-    clearErrorMessage();
-  });
-
-  document.getElementById('show-login').addEventListener('click', (event) => {
-    event.preventDefault();
-    document.getElementById('register-form').style.display = 'none';
-    document.getElementById('login-form').style.display = 'block';
-    clearErrorMessage();
-  });
-
-  togglePasswordVisibility('register-password', 'register-eye-icon');
-  togglePasswordVisibility('login-password', 'eye-icon');
 });
 
-function togglePasswordVisibility(inputId, eyeIconId) {
-  const passwordInput = document.getElementById(inputId);
-  const eyeIcon = document.getElementById(eyeIconId);
-  eyeIcon.addEventListener('click', () => {
-    const isPasswordVisible = passwordInput.type === 'text';
-    passwordInput.type = isPasswordVisible ? 'password' : 'text';
-    eyeIcon.setAttribute('name', isPasswordVisible ? 'eye-outline' : 'eye-off-outline');
-  });
+function onYouTubeIframeAPIReady() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const videoId = urlParams.get('videoId') || 'xiN4EOqpvwc'; // ID padrão caso não haja um na URL
+
+    player = new YT.Player('music-player', {
+        height: '100%',
+        width: '100%',
+        videoId: videoId,
+        playerVars: {
+            listType: 'playlist',
+            list: 'PLX_YaKXOr1s6u6O3srDxVJn720Zi2RRC5',
+            autoplay: 0,
+            controls: 0
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
 }
 
-function displayErrorMessage(message) {
-  const errorMessageElement = document.getElementById('error-message');
-  errorMessageElement.textContent = message;
+function onPlayerReady(event) {
+    setVideoQuality(minQuality); // Define a qualidade inicial para 'medium'
+    setupControlButtons();
+
+    setInterval(() => {
+        if (player && player.getCurrentTime) {
+            const currentTime = player.getCurrentTime();
+            const duration = player.getDuration();
+            if (duration > 0) {
+                progressBar.value = (currentTime / duration) * 100;
+                currentTimeDisplay.textContent = formatTime(currentTime);
+                durationDisplay.textContent = formatTime(duration);
+            }
+        }
+    }, 1000);
+
+    progressBar.addEventListener('input', function() {
+        const duration = player.getDuration();
+        player.seekTo((progressBar.value / 100) * duration, true);
+    });
+
+    const savedTheme = localStorage.getItem('theme');
+    const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+
+    if (savedTheme) {
+        document.documentElement.setAttribute('data-theme', savedTheme);
+        document.body.classList.toggle('dark-mode', savedTheme === 'dark');
+        document.getElementById('theme-toggle').innerHTML = savedTheme === 'dark' ? '<ion-icon name="sunny-outline"></ion-icon>' : '<ion-icon name="moon-outline"></ion-icon>';
+        metaThemeColor.setAttribute('content', savedTheme === 'dark' ? '#13051f' : '#f0f4f9');
+    } else {
+        // Apply dark theme by default
+        document.documentElement.setAttribute('data-theme', 'dark');
+        document.body.classList.add('dark-mode');
+        document.getElementById('theme-toggle').innerHTML = '<ion-icon name="sunny-outline"></ion-icon>';
+        metaThemeColor.setAttribute('content', '#13051f');
+        localStorage.setItem('theme', 'dark');
+    }
+
+    document.getElementById('theme-toggle').addEventListener('click', function() {
+        const currentTheme = document.documentElement.getAttribute('data-theme');
+        if (currentTheme === 'dark') {
+            document.documentElement.setAttribute('data-theme', 'light');
+            document.body.classList.remove('dark-mode');
+            this.innerHTML = '<ion-icon name="moon-outline"></ion-icon>';
+            metaThemeColor.setAttribute('content', '#f0f4f9');
+            localStorage.setItem('theme', 'light');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            document.body.classList.add('dark-mode');
+            this.innerHTML = '<ion-icon name="sunny-outline"></ion-icon>';
+            metaThemeColor.setAttribute('content', '#13051f');
+            localStorage.setItem('theme', 'dark');
+        }
+    });
+
+    fetchPlaylistData();
 }
 
-function clearErrorMessage() {
-  const errorMessageElement = document.getElementById('error-message');
-  errorMessageElement.textContent = '';
+function setupControlButtons() {
+    document.querySelector('.control-button:nth-child(3)').addEventListener('click', function() {
+        if (isPlaying) {
+            player.pauseVideo();
+            this.innerHTML = '<ion-icon name="play-circle-outline" class="play-outline"></ion-icon>';
+        } else {
+            player.playVideo();
+            this.innerHTML = '<ion-icon name="pause-circle-outline" class="pause-outline"></ion-icon>';
+        }
+        isPlaying = !isPlaying;
+    });
+
+    document.querySelector('.control-button:nth-child(2)').addEventListener('click', function() {
+        player.previousVideo();
+    });
+
+    document.querySelector('.control-button:nth-child(4)').addEventListener('click', function() {
+        player.nextVideo();
+    });
+
+    document.querySelector('.control-button:nth-child(1)').addEventListener('click', function() {
+        switch (mode) {
+            case 'repeat':
+                mode = 'repeat_one';
+                this.innerHTML = '<ion-icon name="repeat-outline"></ion-icon><span class="repeat-number">1</span>';
+                break;
+            case 'repeat_one':
+                mode = 'shuffle';
+                this.innerHTML = '<ion-icon name="shuffle-outline"></ion-icon>';
+                isShuffle = true;
+                break;
+            case 'shuffle':
+                mode = 'repeat';
+                this.innerHTML = '<ion-icon name="repeat-outline"></ion-icon>';
+                isShuffle = false;
+                break;
+        }
+    });
+
+    document.querySelector('.control-button:nth-child(5)').addEventListener('click', function() {
+        document.getElementById('playlist-overlay').style.display = 'flex';
+        renderPlaylist(playlistData);
+    });
+
+    document.getElementById('close-playlist').addEventListener('click', function() {
+        document.getElementById('playlist-overlay').style.display = 'none';
+    });
 }
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.ENDED) {
+        document.querySelector('.control-button:nth-child(3)').innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+        isPlaying = false;
+
+        switch (mode) {
+            case 'repeat_one':
+                player.seekTo(0);
+                player.playVideo();
+                break;
+            case 'shuffle':
+                const playlist = player.getPlaylist();
+                const nextIndex = Math.floor(Math.random() * playlist.length);
+                player.playVideoAt(nextIndex);
+                break;
+            case 'repeat':
+                const currentIndex = player.getPlaylistIndex();
+                if (currentIndex === player.getPlaylist().length - 1) {
+                    player.playVideoAt(0);
+                } else {
+                    player.nextVideo();
+                }
+                break;
+        }
+    }
+    updateTitleAndArtist();
+}
+
+function updateTitleAndArtist() {
+    const videoData = player.getVideoData();
+    document.getElementById('title').textContent = videoData.title;
+    document.getElementById('artist').textContent = videoData.author;
+}
+
+function formatTime(seconds) {
+    const min = Math.floor(seconds / 60);
+    const sec = Math.floor(seconds % 60);
+    return `${min}:${sec < 10 ? '0' : ''}${sec}`;
+}
+
+async function fetchPlaylistData() {
+    const playlist = player.getPlaylist();
+    playlistData = playlist.map((videoId, index) => ({
+        videoId,
+        index,
+        title: '',
+        author: ''
+    }));
+
+    for (let i = 0; i < playlistData.length; i++) {
+        const videoId = playlistData[i].videoId;
+        try {
+            const response = await fetch(`https://noembed.com/embed?url=https://www.youtube.com/watch?v=${videoId}`);
+            const data = await response.json();
+            playlistData[i].title = data.title;
+            playlistData[i].author = data.author_name;
+        } catch (error) {
+            console.error('Error fetching video details:', error);
+        }
+    }
+
+    renderPlaylist(playlistData);
+}
+
+function renderPlaylist(playlist) {
+    const playlistContainer = document.getElementById('playlist-items');
+    playlistContainer.innerHTML = '';
+
+    playlist.forEach(video => {
+        const listItem = document.createElement('li');
+
+        const thumbnail = document.createElement('img');
+        thumbnail.src = `https://img.youtube.com/vi/${video.videoId}/default.jpg`;
+        listItem.appendChild(thumbnail);
+
+        const textContainer = document.createElement('div');
+        textContainer.className = 'text-container';
+
+        const titleText = document.createElement('span');
+        titleText.className = 'title';
+        titleText.textContent = video.title;
+        textContainer.appendChild(titleText);
+
+        const authorText = document.createElement('span');
+        authorText.className = 'author';
+        authorText.textContent = video.author;
+        textContainer.appendChild(authorText);
+
+        listItem.appendChild(textContainer);
+
+        listItem.addEventListener('click', () => {
+            if (isShuffle) {
+                // Encontrar o índice correspondente ao vídeo clicado na lista original
+                const originalIndex = playlistData.findIndex(item => item.videoId === video.videoId);
+                player.playVideoAt(originalIndex);
+            } else {
+                player.playVideoAt(video.index);
+            }
+            document.getElementById('playlist-overlay').style.display = 'none';
+        });
+
+        playlistContainer.appendChild(listItem);
+    });
+}
+// BUSCA CONFIG
+
+// Adicione o evento de keyup ao input de texto
+document.getElementById('search-input').addEventListener('keyup', function(event) {
+    const searchText = event.target.value.toLowerCase();
+    const filteredPlaylist = filterPlaylist(searchText);
+    renderPlaylist(filteredPlaylist);
+});
+
+// Crie a função que filtre a playlist
+function filterPlaylist(searchText) {
+    return playlistData.filter(video => video.title.toLowerCase().includes(searchText) || video.author.toLowerCase().includes(searchText));
+}
+
+// SHARE CONFIG
+// Compartilhamento
+document.getElementById('share-icon').addEventListener('click', function() {
+    const videoData = player.getVideoData();
+    const videoId = videoData.video_id;
+    const shareUrl = `https://lovesongsapp.github.io/?videoId=${videoId}`;
+
+    if (navigator.share) {
+        navigator.share({
+            title: videoData.title,
+            text: `🩷💚 Confira este vídeo: ${videoData.title}`,
+            url: shareUrl,
+        }).then(() => {
+            console.log('Compartilhamento bem-sucedido');
+        }).catch((error) => {
+            console.error('Erro ao compartilhar:', error);
+        });
+    } else {
+        // Fallback para navegadores que não suportam a API de compartilhamento
+        alert(`🩷💚 Confira este vídeo: ${videoData.title}\n${shareUrl}`);
+    }
+});
