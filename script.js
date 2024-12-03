32c896c8cb2f23544b363106272d28c46b6105b7
LoveSongs <157166083+lovesongsapp@users.noreply.github.com>
2024-12-03 10:14:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,330 +1,207 @@
 let player;
-let maxQuality = 'medium'; // Definir resolução máxima
-let minQuality = 'low'; // Definir resolução mínima
+const settings = {
+    maxQuality: 'medium',
+    minQuality: 'low',
+    themeColors: { dark: '#13051f', light: '#f0f4f9' },
+    playlistId: 'PLX_YaKXOr1s6u6O3srDxVJn720Zi2RRC5',
+    initialVideoId: 'eT5_neXR3FI'
+};
 let isPlaying = false;
-let isShuffle = false;
 let mode = 'repeat'; // 'repeat', 'repeat_one', 'shuffle'
-let progressBar, currentTimeDisplay, durationDisplay;
 let playlistData = [];
-let sharedVideoId = null;
 
-function setVideoQuality(quality) {
-    player.setPlaybackQuality(quality);
-}
+// DOM Elements
+let progressBar, currentTimeDisplay, durationDisplay;
 
-document.addEventListener('DOMContentLoaded', function() {
-    progressBar = document.getElementById('progress');
-    currentTimeDisplay = document.getElementById('current-time');
-    durationDisplay = document.getElementById('duration');
-    
-    // Verifique se todos os elementos DOM necessários estão presentes
-    if (progressBar && currentTimeDisplay && durationDisplay) {
-        onYouTubeIframeAPIReady();
-    } else {
-        console.error('Um ou mais elementos DOM não foram encontrados.');
+// Helper: Format time (mm:ss)
+const formatTime = (seconds) => {
+    const min = Math.floor(seconds / 60);
+    const sec = Math.floor(seconds % 60);
+    return `${min}:${sec < 10 ? '0' : ''}${sec}`;
+};
+
+// Helper: Fetch video details
+const fetchVideoDetails = async (videoId) => {
+    try {
+        const response = await fetch(`https://noembed.com/embed?url=https://www.youtube.com/watch?v=${videoId}`);
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching video details:', error);
+        return null;
     }
-});
+};
 
+// Fetch playlist data
+const fetchPlaylistData = async () => {
+    const playlist = player.getPlaylist();
+    playlistData = await Promise.all(
+        playlist.map(async (videoId, index) => {
+            const videoDetails = await fetchVideoDetails(videoId);
+            return {
+                videoId,
+                index,
+                title: videoDetails?.title || 'Unknown Title',
+                author: videoDetails?.author_name || 'Unknown Author'
+            };
+        })
+    );
+    renderPlaylist(playlistData);
+};
+
+// Render playlist
+const renderPlaylist = (videos) => {
+    const playlistContainer = document.getElementById('playlist-items');
+    playlistContainer.innerHTML = '';
+
+    videos.forEach((video) => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+            <img src="https://img.youtube.com/vi/${video.videoId}/default.jpg" alt="${video.title}">
+            <div class="text-container">
+                <span class="title">${video.title}</span>
+                <span class="author">${video.author}</span>
+            </div>
+        `;
+        listItem.addEventListener('click', () => {
+            player.playVideoAt(video.index);
+            document.getElementById('playlist-overlay').style.display = 'none';
+        });
+        playlistContainer.appendChild(listItem);
+    });
+};
+
+// Update progress bar and time displays
+const updateProgressBar = () => {
+    if (player && player.getCurrentTime) {
+        const currentTime = player.getCurrentTime();
+        const duration = player.getDuration();
+        if (duration > 0) {
+            progressBar.value = (currentTime / duration) * 100;
+            currentTimeDisplay.textContent = formatTime(currentTime);
+            durationDisplay.textContent = formatTime(duration);
+        }
+    }
+};
+
+// Set video quality
+const setVideoQuality = (quality) => player.setPlaybackQuality(quality);
+
+// Initialize YouTube Player
 function onYouTubeIframeAPIReady() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const videoId = urlParams.get('videoId') || 'eT5_neXR3FI'; // Video Inicial da Playlist
+    const videoId = new URLSearchParams(window.location.search).get('videoId') || settings.initialVideoId;
 
     player = new YT.Player('music-player', {
         height: '100%',
         width: '100%',
-        videoId: videoId,
+        videoId,
         playerVars: {
-            'listType': 'playlist',
-            'list': 'PLX_YaKXOr1s6u6O3srDxVJn720Zi2RRC5',
-            'autoplay': 0,
-            'controls': 0,
-            'iv_load_policy': 3,
-            'modestbranding': 1,
-            'rel': 0 // Evita mostrar vídeos relacionados ao final
+            listType: 'playlist',
+            list: settings.playlistId,
+            autoplay: 0,
+            controls: 0,
+            iv_load_policy: 3,
+            modestbranding: 1,
+            rel: 0
         },
         events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
+            onReady: onPlayerReady,
+            onStateChange: onPlayerStateChange
         }
     });
 }
 
-function onPlayerReady(event) {
-    setVideoQuality(minQuality); // Define a qualidade inicial para 'medium'
-    setupControlButtons();
-// Controle de Volume
-    const volumeControl = document.getElementById('volume-control');
-    
-    // Verifica se o controle de volume existe no DOM
-    if (volumeControl) {
-        player.setVolume(100); // Define volume inicial em 100%
-        
-        // Atualiza o volume do player ao mover o controle
-        volumeControl.addEventListener('input', function() {
-            const volume = parseInt(volumeControl.value, 10); // Obtém o valor do controle
-            player.setVolume(volume); // Aplica o volume no player (intervalo 0 a 100)
-        });
-    } else {
-        console.error('Controle de volume não encontrado no DOM.');
-    }
+// On player ready
+function onPlayerReady() {
+    setVideoQuality(settings.minQuality);
+    initializeControls();
+    fetchPlaylistData();
 
-    setInterval(() => {
-        if (player && player.getCurrentTime) {
-            const currentTime = player.getCurrentTime();
-            const duration = player.getDuration();
-            if (duration > 0) {
-                progressBar.value = (currentTime / duration) * 100;
-                currentTimeDisplay.textContent = formatTime(currentTime);
-                durationDisplay.textContent = formatTime(duration);
-            }
-        }
-    }, 1000);
+    setInterval(updateProgressBar, 1000);
 
-    progressBar.addEventListener('input', function() {
+    progressBar.addEventListener('input', () => {
         const duration = player.getDuration();
         player.seekTo((progressBar.value / 100) * duration, true);
     });
-
- const savedTheme = localStorage.getItem('theme');
-const metaThemeColor = document.querySelector('meta[name="theme-color"]');
-const themeToggleIcon = document.querySelector('#theme-toggle ion-icon');
-
-if (savedTheme) {
-    document.documentElement.setAttribute('data-theme', savedTheme);
-    document.body.classList.toggle('dark-mode', savedTheme === 'dark');
-    themeToggleIcon.setAttribute('name', savedTheme === 'dark' ? 'sunny-outline' : 'moon-outline');
-    metaThemeColor.setAttribute('content', savedTheme === 'dark' ? '#13051f' : '#f0f4f9');
-} else {
-    // Apply dark theme by default
-    document.documentElement.setAttribute('data-theme', 'dark');
-    document.body.classList.add('dark-mode');
-    themeToggleIcon.setAttribute('name', 'sunny-outline');
-    metaThemeColor.setAttribute('content', '#13051f');
-    localStorage.setItem('theme', 'dark');
 }
 
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    const currentTheme = document.documentElement.getAttribute('data-theme');
-    if (currentTheme === 'dark') {
-        document.documentElement.setAttribute('data-theme', 'light');
-        document.body.classList.remove('dark-mode');
-        themeToggleIcon.setAttribute('name', 'moon-outline');
-        metaThemeColor.setAttribute('content', '#f0f4f9');
-        localStorage.setItem('theme', 'light');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        document.body.classList.add('dark-mode');
-        themeToggleIcon.setAttribute('name', 'sunny-outline');
-        metaThemeColor.setAttribute('content', '#13051f');
-        localStorage.setItem('theme', 'dark');
-    }
-});
+// Initialize control buttons
+const initializeControls = () => {
+    const controls = {
+        playPause: document.querySelector('.control-button.play-pause'),
+        previous: document.querySelector('.control-button.previous'),
+        next: document.querySelector('.control-button.next'),
+        modeToggle: document.querySelector('.control-button.mode'),
+        playlist: document.querySelector('.control-button.playlist')
+    };
 
-    fetchPlaylistData();
-}
-
-function setupControlButtons() {
-    document.querySelector('.control-button:nth-child(3)').addEventListener('click', function() {
+    controls.playPause.addEventListener('click', () => {
         if (isPlaying) {
             player.pauseVideo();
-            this.innerHTML = '<ion-icon name="play-circle-outline" class="play-outline"></ion-icon>';
+            controls.playPause.innerHTML = '<ion-icon name="play-circle-outline"></ion-icon>';
         } else {
             player.playVideo();
-            this.innerHTML = '<ion-icon name="pause-circle-outline" class="pause-outline"></ion-icon>';
+            controls.playPause.innerHTML = '<ion-icon name="pause-circle-outline"></ion-icon>';
         }
         isPlaying = !isPlaying;
     });
 
-    document.querySelector('.control-button:nth-child(2)').addEventListener('click', function() {
-        player.previousVideo();
-    });
+    controls.previous.addEventListener('click', () => player.previousVideo());
+    controls.next.addEventListener('click', () => player.nextVideo());
 
-    document.querySelector('.control-button:nth-child(4)').addEventListener('click', function() {
-        player.nextVideo();
-    });
-
-    document.querySelector('.control-button:nth-child(1)').addEventListener('click', function() {
+    controls.modeToggle.addEventListener('click', () => {
         switch (mode) {
             case 'repeat':
                 mode = 'repeat_one';
-                this.innerHTML = '<ion-icon name="repeat-outline"></ion-icon><span class="repeat-number">1</span>';
+                controls.modeToggle.innerHTML = '<ion-icon name="repeat-outline"></ion-icon><span class="repeat-number">1</span>';
                 break;
             case 'repeat_one':
                 mode = 'shuffle';
-                this.innerHTML = '<ion-icon name="shuffle-outline"></ion-icon>';
-                isShuffle = true;
+                controls.modeToggle.innerHTML = '<ion-icon name="shuffle-outline"></ion-icon>';
                 break;
             case 'shuffle':
                 mode = 'repeat';
-                this.innerHTML = '<ion-icon name="repeat-outline"></ion-icon>';
-                isShuffle = false;
+                controls.modeToggle.innerHTML = '<ion-icon name="repeat-outline"></ion-icon>';
                 break;
         }
     });
 
-    document.querySelector('.control-button:nth-child(5)').addEventListener('click', function() {
+    controls.playlist.addEventListener('click', () => {
         document.getElementById('playlist-overlay').style.display = 'flex';
         renderPlaylist(playlistData);
     });
 
-    document.getElementById('close-playlist').addEventListener('click', function() {
+    document.getElementById('close-playlist').addEventListener('click', () => {
         document.getElementById('playlist-overlay').style.display = 'none';
     });
 }
 
+// Handle player state change
 function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.ENDED) {
-        document.querySelector('.control-button:nth-child(3)').innerHTML = '<ion-icon name="play-outline"></ion-icon>';
+    if (event.data === YT.PlayerState.ENDED) {
         isPlaying = false;
+        document.querySelector('.control-button.play-pause').innerHTML = '<ion-icon name="play-circle-outline"></ion-icon>';
 
-        switch (mode) {
-            case 'repeat_one':
-                player.seekTo(0);
-                player.playVideo();
-                break;
-            case 'shuffle':
-                const playlist = player.getPlaylist();
-                const nextIndex = Math.floor(Math.random() * playlist.length);
-                player.playVideoAt(nextIndex);
-                break;
-            case 'repeat':
-                const currentIndex = player.getPlaylistIndex();
-                if (currentIndex === player.getPlaylist().length - 1) {
-                    player.playVideoAt(0);
-                } else {
-                    player.nextVideo();
-                }
-                break;
+        if (mode === 'repeat_one') {
+            player.seekTo(0);
+            player.playVideo();
+        } else if (mode === 'shuffle') {
+            const nextIndex = Math.floor(Math.random() * player.getPlaylist().length);
+            player.playVideoAt(nextIndex);
+        } else {
+            player.nextVideo();
         }
     }
-    updateTitleAndArtist();
 }
 
-function updateTitleAndArtist() {
-    const videoData = player.getVideoData();
-    document.getElementById('title').textContent = videoData.title;
-    document.getElementById('artist').textContent = videoData.author;
-}
+// Document ready
+document.addEventListener('DOMContentLoaded', () => {
+    progressBar = document.getElementById('progress');
+    currentTimeDisplay = document.getElementById('current-time');
+    durationDisplay = document.getElementById('duration');
 
-function formatTime(seconds) {
-    const min = Math.floor(seconds / 60);
-    const sec = Math.floor(seconds % 60);
-    return `${min}:${sec < 10 ? '0' : ''}${sec}`;
-}
-
-async function fetchPlaylistData() {
-    const playlist = player.getPlaylist();
-    playlistData = playlist.map((videoId, index) => ({
-        videoId,
-        index,
-        title: '',
-        author: ''
-    }));
-
-    for (let i = 0; i < playlistData.length; i++) {
-        const videoId = playlistData[i].videoId;
-        try {
-            const response = await fetch(`https://noembed.com/embed?url=https://www.youtube.com/watch?v=${videoId}`);
-            const data = await response.json();
-            playlistData[i].title = data.title;
-            playlistData[i].author = data.author_name;
-        } catch (error) {
-            console.error('Error fetching video details:', error);
-        }
-    }
-
-    renderPlaylist(playlistData);
-}
-
-// inicio
-
-function renderPlaylist(videos) {
-    const playlistContainer = document.getElementById('playlist-items');
-    playlistContainer.innerHTML = ''; // Limpa a lista atual
-
-    videos.forEach(video => {
-        const listItem = document.createElement('li');
-
-        // Cria a miniatura do vídeo
-        const thumbnail = document.createElement('img');
-        thumbnail.src = `https://img.youtube.com/vi/${video.videoId}/default.jpg`;
-        listItem.appendChild(thumbnail);
-
-        // Cria um contêiner para o texto
-        const textContainer = document.createElement('div');
-        textContainer.className = 'text-container';
-
-        // Cria e adiciona o título
-        const titleText = document.createElement('span');
-        titleText.className = 'title';
-        titleText.textContent = video.title;
-        textContainer.appendChild(titleText);
-
-        // Cria e adiciona o autor
-        const authorText = document.createElement('span');
-        authorText.className = 'author';
-        authorText.textContent = video.author;
-        textContainer.appendChild(authorText);
-
-        // Adiciona o contêiner de texto ao item da lista
-        listItem.appendChild(textContainer);
-
-        // Adiciona o evento de clique
-        listItem.addEventListener('click', () => {
-            if (isShuffle) {
-                // Encontrar o índice correspondente ao vídeo clicado na lista original
-                const originalIndex = playlistData.findIndex(item => item.videoId === video.videoId);
-                player.playVideoAt(originalIndex);
-            } else {
-                player.playVideoAt(video.index);
-            }
-            document.getElementById('playlist-overlay').style.display = 'none';
-        });
-
-        // Adiciona o item configurado à lista de reprodução
-        playlistContainer.appendChild(listItem);
-    });
-}
-
-// Renderiza a playlist completa ao carregar a página
-renderPlaylist(playlistData);
-
-// Função para filtrar a playlist
-function filterPlaylist(searchText) {
-    return playlistData.filter(video => {
-        const titleMatch = video.title && video.title.toLowerCase().includes(searchText);
-        const authorMatch = video.author && video.author.toLowerCase().includes(searchText);
-        return titleMatch || authorMatch;
-    });
-}
-
-// Configuração da busca
-const searchInput = document.getElementById('search-input');
-searchInput.addEventListener('input', () => {
-    const searchText = searchInput.value.trim().toLowerCase();
-    const filteredPlaylist = filterPlaylist(searchText);
-    renderPlaylist(filteredPlaylist);
-});
-
-
-// Compartilhamento
-document.getElementById('share-icon').addEventListener('click', function() {
-    const videoData = player.getVideoData();
-    const videoId = videoData.video_id;
-    const shareUrl = `https://lovesongsapp.github.io/?videoId=${videoId}`;
-
-    if (navigator.share) {
-        navigator.share({
-            title: videoData.title,
-            text: `🥰 LoveSongs: ${videoData.title}`,
-            url: shareUrl,
-        }).then(() => {
-            console.log('Compartilhamento bem-sucedido');
-        }).catch((error) => {
-            console.error('Erro ao compartilhar:', error);
-        });
+    if (progressBar && currentTimeDisplay && durationDisplay) {
+        onYouTubeIframeAPIReady();
     } else {
-        // Fallback para navegadores que não suportam a API de compartilhamento
-        alert(`🩷💚 Confira este vídeo: ${videoData.title}\n${shareUrl}`);
+        console.error('Essential DOM elements missing!');
     }
 });
