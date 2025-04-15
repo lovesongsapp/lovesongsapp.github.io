29c7853181ce13994166106f39d3d828d5b705c7
LoveSongs <157166083+lovesongsapp@users.noreply.github.com>
2025-04-15 17:05:24 -0400

    Update sucesso.js

--- scripts/sucesso.js
+++ scripts/sucesso.js
@@ -6,8 +6,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyCOed1uYtjzkR1OpYS6z3-sbIVPQW6MohM",
   authDomain: "lovesongs-1285e.firebaseapp.com",
   projectId: "lovesongs-1285e",
-  storageBucket: "SEU_BUCKET.appspot.com",
-  messagingSenderId: "SEU_ID",
+  storageBucket: "lovesongs-1285e.firebasestorage.app",
+  messagingSenderId: "940749066428",
   appId: "1:940749066428:web:4bc067e8721fa576fe40b9",
 };
 
