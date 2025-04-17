5011f2dc2e688c88211ebdcc05c201804c797b5b
LoveSongs <157166083+lovesongsapp@users.noreply.github.com>
2025-04-17 07:48:07 -0400

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -92,3 +92,29 @@ document.getElementById("forgotPassword").addEventListener("click", () => {
     })
     .catch(err => alert("Erro: " + err.message));
 });
+
+//email Manual e Alertas
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+document.getElementById("register").addEventListener("click", () => {
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+
+  if (email === "" || pass === "") {
+    alert("Preencha todos os campos para se cadastrar.");
+    return;
+  }
+
+  createUserWithEmailAndPassword(auth, email, pass)
+    .then(() => {
+      alert("Conta criada com sucesso! Você já pode fazer login.");
+      // window.location.href = "login.html";
+    })
+    .catch(err => {
+      if (err.code === "auth/email-already-in-use") {
+        alert("Este e-mail já está em uso. Tente recuperar a senha.");
+      } else {
+        alert("Erro ao cadastrar: " + err.message);
+      }
+    });
+});
