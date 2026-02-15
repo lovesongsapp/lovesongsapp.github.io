859d4dd848f370fd8ffb582277fa7fa78b4c1aa3
LoveSongs <157166083+lovesongsapp@users.noreply.github.com>
2026-02-15 04:09:57 -0400

    Update service worker cache version and logic

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lovesongsapp-cache-v2.1';
+const CACHE_NAME = 'lovesongsapp-cache-v2.2';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -141,5 +141,6 @@ self.addEventListener('message', event => {
     self.skipWaiting();
   }
 });
+
 
 
